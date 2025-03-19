bf8c04a34ee4e2b68a6babfd05b69df03b38c465
Mitlit <albatrosstebex@gmail.com>
2025-03-19 21:10:40 +1300

    added config & fixed description

--- src/main/java/it/mitl/vpnutils/VPNUtils.java
+++ src/main/java/it/mitl/vpnutils/VPNUtils.java
@@ -3,6 +3,7 @@ package it.mitl.vpnutils;
 import it.mitl.vpnutils.Listeners.OnJoin;
 import it.mitl.vpnutils.utils.APIChanges;
 import org.bstats.bukkit.Metrics;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class VPNUtils extends JavaPlugin {
@@ -10,6 +11,10 @@ public final class VPNUtils extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+
+        // Configuration Setup
+        saveDefaultConfig();
+        FileConfiguration config = getConfig();
 
         // bStats
         int pluginId = 25155;
