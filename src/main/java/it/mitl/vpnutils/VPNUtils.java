5e8fc7d64d367220d1cdea8a5a423ef6bbb7cd38
Mitlit <albatrosstebex@gmail.com>
2025-03-19 20:59:39 +1300

    added bStats

--- src/main/java/it/mitl/vpnutils/VPNUtils.java
+++ src/main/java/it/mitl/vpnutils/VPNUtils.java
@@ -2,6 +2,7 @@ package it.mitl.vpnutils;
 
 import it.mitl.vpnutils.Listeners.OnJoin;
 import it.mitl.vpnutils.utils.APIChanges;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class VPNUtils extends JavaPlugin {
@@ -9,6 +10,10 @@ public final class VPNUtils extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+
+        // bStats
+        int pluginId = 25155;
+        Metrics metrics = new Metrics(this, pluginId);
 
         // Check if the API version is still supported.
         if (!APIChanges.versionSupported(this)) {
